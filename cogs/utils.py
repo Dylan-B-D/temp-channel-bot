00dbda6d7cd3667a4edb74d90d57f633672d96c4
D <dylanduretz@gmail.com>
2024-01-14 21:11:49 +0000

    Update help command

--- cogs/utils.py
+++ cogs/utils.py
@@ -9,12 +9,18 @@ class Utils(commands.Cog):
 
     @commands.command()
     async def help(self, ctx):
-        embed = Embed(title="Help", description="List of commands and their usages:", color=discord.Color.blue())
+        embed = discord.Embed(title="Help", description="List of commands and their usages:", color=discord.Color.blue())
 
         # settempchannel command details
         embed.add_field(name="!settempchannel", value="Sets the current channel as a temporary channel.", inline=False)
 
         # setcleartime command details
         embed.add_field(name="!setcleartime [minutes]", value="Sets the clear time for messages in the current temp channel. Value is in minutes.", inline=False)
+
+        # info command details
+        embed.add_field(name="!info", value="View temporary channels in the server and the clear time for these channels.", inline=False)
+
+        # removetempchannel command details
+        embed.add_field(name="!removetempchannel", value="Removes the current channel from being a temporary channel.", inline=False)
 
         await ctx.send(embed=embed)
